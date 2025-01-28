472663e206d85c2c82d4dfb04b30bf1ccd0703f7
pshemssa <122387932+pshemssa@users.noreply.github.com>
2025-01-28 17:15:13 +0000

    react state

---
+++ react_state/task_2/dashboard/src/utils/utils.js
@@ -0,0 +1,16 @@
+export function getFullYear() {
+    return new Date().getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+    if (isIndex) {
+        return 'Holberton School.';
+    }
+    else {
+        return 'Holberton School main dashboard';
+    }
+}
+
+export function getLatestNotification() {
+    return {__html: '<strong>Urgent requirement</strong> - complete by EOD'};
+}
---
+++ react_state/task_2/dashboard/src/utils/utils.test.js
@@ -0,0 +1,20 @@
+import { strict as assert } from 'assert';
+import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
+
+describe('Test Utils', () => {
+
+    it('Tests that getFullYear is current', () => {
+        assert.equal(getFullYear(), new Date().getFullYear());
+    });
+    it('Validates the result of getFooterCopy with true', () => {
+        assert.equal(getFooterCopy(true), 'Holberton School.');
+    });
+    it('Validates the result of getFooterCopy with false', () => {
+        assert.equal(getFooterCopy(false), 'Holberton School main dashboard');
+    });
+
+    it('checks return of getLatestNotification', () => {
+        // use JSON.stringify because
+        assert.equal(JSON.stringify(getLatestNotification()), JSON.stringify({ __html: '<strong>Urgent requirement</strong> - complete by EOD' }));
+    });
+});
